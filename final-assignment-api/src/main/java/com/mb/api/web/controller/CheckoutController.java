688854ae33cc35a296a10986bda39d6ce8d9149e
SachinGaikwadMB <sachin.gaikwad@mindbowser.com>
2022-07-28 09:45:29 +0530

    initial commit

--- final-assignment-api/src/main/java/com/mb/api/web/controller/CheckoutController.java
+++ final-assignment-api/src/main/java/com/mb/api/web/controller/CheckoutController.java
@@ -1,8 +1,12 @@
 package com.mb.api.web.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,13 +17,24 @@ import com.stripe.exception.StripeException;
 import com.stripe.model.PaymentIntent;
 
 @RestController
-@RequestMapping("/api/checkout")
+@RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:4200/")
 public class CheckoutController
 {
 	@Autowired
 	private CheckoutService checkoutService;
-
-	@PostMapping("/payment-intent")
+	
+	@GetMapping("/test")
+	public List<String> test() {
+		List<String> list = new ArrayList<>();
+		list.add("sachin");
+		list.add("aman");
+		list.add("amaresh");
+		
+		return list;
+	}
+	
+	@PostMapping("/checkout/payment-intent")
 	public ResponseEntity<String> createPaymentIntent(@RequestBody PaymentInfoDto paymentInfoDto) throws StripeException
 	{
 		PaymentIntent paymentIntent = checkoutService.createPaymentIntent(paymentInfoDto);
